29999b494382f295da29306c79856970f06ebf4b
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-26 19:03:46 +0900

    順番表示画面実装(最低限)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,10 +9,14 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_to root_path
+      render :table
     else
       render :new
     end
+  end
+
+  def table
+    @player = Player.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "players#index"
-  resources :players, only: [:index, :new, :create]
+  resources :players do
+    member do
+      get 'table'
+    end
+  end
 end

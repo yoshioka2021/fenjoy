7fd13844894d9f4b200f49bbef211cbd01f0d44b
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-26 16:18:25 +0900

    フォームの作成と保存(最低限)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,21 @@
 class PlayersController < ApplicationController
   def index
   end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:number,:name1,:name2,:name3,:name4,:name5,:name6)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "players#index"
+  resources :players, only: [:index, :new, :create]
 end

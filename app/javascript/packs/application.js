4c4e0837916e50f92a993bd9c0978f131aca2587
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-05-08 12:34:07 +0900

    jsファイル記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+<head>
+  <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+</head>
 
-
+html2canvas(document.querySelector("#canvas-box")).then(canvas => { 
+    let downloadEle = document.createElement("table");
+    donwloadEle.href = canvas.toDataURL("image/png");
+    donloadEle.download = "canvas.png";
+    downloadEle.click();
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
